e2acc8a185514397df6321fe3276f815352f02e1
Joao Paiva <jgpaiva@gmail.com>
2014-07-14 18:32:50 +0100

    use tests with correct argument order

--- src/test/java/com/unbabel/sdk/ApiTest.java
+++ src/test/java/com/unbabel/sdk/ApiTest.java
@@ -14,7 +14,7 @@ public class ApiTest {
    public void testTranslation() throws IOException {
       String raw_translation = Utils.fileToString("translation.json");
       Translation translation = Translation.fromJSON(raw_translation);
-      assertEquals(translation.uid, "89500a6e77");
+      assertEquals("89500a6e77", translation.uid);
    }
 
    @Test
@@ -22,11 +22,11 @@ public class ApiTest {
       String lang_pairs_raw = Utils.fileToString("lang_pair.json");
       ArrayList<LangPair> lang_pairs = Utils.decodeObjectArray(lang_pairs_raw,
             "lang_pair", LangPair.class);
-      assertEquals(lang_pairs.size(), 2);
-      assertEquals(lang_pairs.get(0).sourceLanguage.shortName, "pt");
-      assertEquals(lang_pairs.get(0).targetLanguage.shortName, "en");
-      assertEquals(lang_pairs.get(1).sourceLanguage.shortName, "de");
-      assertEquals(lang_pairs.get(1).targetLanguage.shortName, "pt");
+      assertEquals(2, lang_pairs.size());
+      assertEquals("pt", lang_pairs.get(0).sourceLanguage.shortName);
+      assertEquals("en", lang_pairs.get(0).targetLanguage.shortName);
+      assertEquals("de", lang_pairs.get(1).sourceLanguage.shortName);
+      assertEquals("pt", lang_pairs.get(1).targetLanguage.shortName);
    }
 
    @Test
@@ -34,8 +34,8 @@ public class ApiTest {
       String tone_raw = Utils.fileToString("tone.json");
       ArrayList<Tone> tone = Utils.decodeObjectArray(tone_raw, "tone",
             Tone.class);
-      assertEquals(tone.size(), 4);
-      assertEquals(tone.get(0).name, "Informal");
+      assertEquals(4, tone.size());
+      assertEquals("Informal", tone.get(0).name);
    }
 
    @Test
@@ -43,8 +43,8 @@ public class ApiTest {
       String topics_raw = Utils.fileToString("topics.json");
       ArrayList<Topic> topics = Utils.decodeObjectArray(topics_raw, "topic",
             Topic.class);
-      assertEquals(topics.size(), 2);
-      assertEquals(topics.get(0).name, "politics");
+      assertEquals(2, topics.size());
+      assertEquals("politics", topics.get(0).name);
    }
 
    @Test
@@ -52,8 +52,8 @@ public class ApiTest {
       String translations_raw = Utils.fileToString("translations.json");
       ArrayList<Translation> translations = Utils.decodeObjectArray(
             translations_raw, "", Translation.class);
-      assertEquals(translations.size(), 2);
-      assertEquals(translations.get(0).uid, "a281dab6e1");
+      assertEquals(2, translations.size());
+      assertEquals("a281dab6e1", translations.get(0).uid);
    }
 
    public static final String TEST_URL = "http://httpbin.org/post";
@@ -70,26 +70,26 @@ public class ApiTest {
       JsonNode rootNode = mapper.readTree(raw_response);
       JsonNode headers = rootNode.get("headers");
       JsonNode accept = headers.get("Accept");
-      assertEquals(accept.asText(), "application/json");
+      assertEquals("application/json", accept.asText());
       JsonNode auth = headers.get("Authorization");
-      assertEquals(auth.asText(), "ApiKey testUser:testKey");
+      assertEquals("ApiKey testUser:testKey", auth.asText());
       JsonNode contentType = headers.get("Content-Type");
-      assertEquals(contentType.asText(), "application/json");
+      assertEquals("application/json", contentType.asText());
       JsonNode json = rootNode.get("json");
-      assertEquals(Translation.fromJSON(json.toString()), t);
+      assertEquals(t, Translation.fromJSON(json.toString()));
    }
    
    @Test
    public void testAccount() throws IOException {
       String account_raw = Utils.fileToString("account.json");
       Account account = Account.fromJSON(account_raw);
-      assertEquals(account.email, "jgpaiva@gmail.com");
+      assertEquals("jgpaiva@gmail.com", account.email);
    }
    
    @Test
    public void testOrder() throws IOException {
       String order_raw = Utils.fileToString("order.json");
       Order order = Order.fromJSON(order_raw);
-      assertEquals(order.status, "New");
+      assertEquals("New", order.status);
    }
 }
